d3fa8ae06eac241a11583e356efd093b61526c03
Thorsten Korpitsch <thorsten.korpitsch@gmail.com>
2020-04-10 15:38:46 +0200

    added graph class

---
+++ src/graph.cpp
@@ -0,0 +1,5 @@
+//
+// Created by thors on 10/04/2020.
+//
+
+#include "graph.h"
---
+++ src/graph.h
@@ -0,0 +1,14 @@
+//
+// Created by thors on 10/04/2020.
+//
+
+#ifndef GROUP2_GPU_CA_SS2020_GRAPH_H
+#define GROUP2_GPU_CA_SS2020_GRAPH_H
+
+
+class graph {
+
+};
+
+
+#endif //GROUP2_GPU_CA_SS2020_GRAPH_H
